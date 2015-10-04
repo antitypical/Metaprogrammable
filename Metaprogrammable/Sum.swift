09fa722a1f297c4c24650b6000f9c45d0b5bed70
Rob Rix <rob.rix@github.com>
2015-10-04 13:41:58 -0400

    Sum is DictionaryLiteralConvertible.

--- Metaprogrammable/Sum.swift
+++ Metaprogrammable/Sum.swift
@@ -1,6 +1,10 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-public enum Sum {
+public enum Sum: DictionaryLiteralConvertible {
 	indirect case Branch(String, Product, Sum)
 	case End
+
+	public init(dictionaryLiteral: (String, Product)...) {
+		self = dictionaryLiteral.fold(.End) { .Branch($0.0, $0.1, $1) }
+	}
 }
