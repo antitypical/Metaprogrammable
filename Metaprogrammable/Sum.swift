ed2a6145515e43890736ff60c5803ab8a651f2ab
Rob Rix <rob.rix@github.com>
2015-10-04 13:52:14 -0400

    Represent Sum branches as label/product pairs.

--- Metaprogrammable/Sum.swift
+++ Metaprogrammable/Sum.swift
@@ -1,10 +1,10 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 public enum Sum: DictionaryLiteralConvertible {
-	indirect case Branch(String, Product, Sum)
+	indirect case Branch((String, Product), Sum)
 	case End
 
 	public init(dictionaryLiteral: (String, Product)...) {
-		self = dictionaryLiteral.fold(.End) { .Branch($0.0, $0.1, $1) }
+		self = dictionaryLiteral.fold(.End, combine: Sum.Branch)
 	}
 }
