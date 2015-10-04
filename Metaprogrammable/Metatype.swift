2f7df0e8130e05bb2ec44240947e24b240282f07
Rob Rix <rob.rix@github.com>
2015-10-04 14:05:30 -0400

    Document Metatype constructors.

--- Metaprogrammable/Metatype.swift
+++ Metaprogrammable/Metatype.swift
@@ -1,7 +1,14 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 public enum Metatype {
+	/// A structural, or algebraic, type.
 	case Structural(Sum)
+
+	/// An opaque reference to a type.
 	case Opaque(Any.Type)
+
+	/// A recursive reference to the type.
+	///
+	/// This can only appear within `Sum`s and `Product`s.
 	case Recurrence
 }
