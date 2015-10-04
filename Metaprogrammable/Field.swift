5f5fde64e2449ce905751734dc4f1e8bc07c078d
Rob Rix <rob.rix@github.com>
2015-10-04 16:15:44 -0400

    Add convenience constructors to Field.

--- Metaprogrammable/Field.swift
+++ Metaprogrammable/Field.swift
@@ -4,4 +4,16 @@ public struct Field {
 	public let label: String?
 	public let type: Any.Type
 	public let getter: () -> Any
+
+	public init<T>(label: String, getter: () -> T) {
+		self.label = label
+		type = T.self
+		self.getter = getter
+	}
+
+	public init<T>(getter: () -> T) {
+		label = nil
+		type = T.self
+		self.getter = getter
+	}
 }
