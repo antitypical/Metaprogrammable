578d077037a7f11561069ec460264cec1c614a47
Rob Rix <rob.rix@github.com>
2015-10-04 12:56:54 -0400

    Add the base files.

---
+++ Metaprogrammable/Metaprogrammable.h
@@ -0,0 +1,8 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+/// Project version number for Metaprogrammable.
+extern double MetaprogrammableVersionNumber;
+
+/// Project version string for Metaprogrammable.
+extern const unsigned char MetaprogrammableVersionString[];
+
---
+++ MetaprogrammableTests/MetaprogrammableTests.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+final class MetaprogrammableTests: XCTestCase {
+	func testX() {
+		//
+	}
+}
+
+
+@testable import Metaprogrammable
+import XCTest
