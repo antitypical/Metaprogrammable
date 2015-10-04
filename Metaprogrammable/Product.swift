466c7c6e9bc2932ee8d3328987fb7b1890e7ee70
Rob Rix <rob.rix@github.com>
2015-10-04 13:40:15 -0400

    Product is ArrayLiteralConvertible.

--- Metaprogrammable/Product.swift
+++ Metaprogrammable/Product.swift
@@ -1,6 +1,10 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-public enum Product {
+public enum Product: ArrayLiteralConvertible {
 	indirect case Field(String, Product)
 	case End
+
+	public init(arrayLiteral: String...) {
+		self = arrayLiteral.fold(.End, combine: Product.Field)
+	}
 }
