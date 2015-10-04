ccb631c839d84a5d1b2e15c5cc621bc6135d95da
Rob Rix <rob.rix@github.com>
2015-10-04 14:07:54 -0400

    Sum is Metaprogrammable.

--- Metaprogrammable/Sum.swift
+++ Metaprogrammable/Sum.swift
@@ -1,10 +1,17 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-public enum Sum: DictionaryLiteralConvertible {
+public enum Sum: DictionaryLiteralConvertible, Metaprogrammable {
 	indirect case Branch((String, Product), Sum)
 	case End
 
 	public init(dictionaryLiteral: (String, Product)...) {
 		self = dictionaryLiteral.fold(.End, combine: Sum.Branch)
+	}
+
+	public static var metatype: Metatype {
+		return .Structural([
+			"Branch": [ (nil, .Opaque(String.self)), (nil, Product.metatype), (nil, .Recurrence) ],
+			"End": []
+		])
 	}
 }
