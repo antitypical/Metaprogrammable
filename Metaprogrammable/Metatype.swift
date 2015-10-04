71d8bf3632ef6490249a310ced5e6b3e0a6f8cfc
Rob Rix <rob.rix@github.com>
2015-10-04 14:09:14 -0400

    Metatype is Metaprogrammable.

--- Metaprogrammable/Metatype.swift
+++ Metaprogrammable/Metatype.swift
@@ -1,7 +1,7 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 /// A description of the structure of a type.
-public enum Metatype {
+public enum Metatype: Metaprogrammable {
 	/// A structural, or algebraic, type.
 	case Structural(Sum)
 
@@ -12,4 +12,13 @@ public enum Metatype {
 	///
 	/// This can only appear within `Sum`s and `Product`s.
 	case Recurrence
+
+
+	public static var metatype: Metatype {
+		return .Structural([
+			"Structural": [ (nil, Sum.metatype) ],
+			"Opaque": [ (nil, .Opaque(Any.Type.self)) ],
+			"Recurrence": []
+		])
+	}
 }
