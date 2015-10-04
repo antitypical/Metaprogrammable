4af53cdefa85450928b399fe2c07a155195d2e1b
Rob Rix <rob.rix@github.com>
2015-10-04 14:05:49 -0400

    Document Metatype.

--- Metaprogrammable/Metatype.swift
+++ Metaprogrammable/Metatype.swift
@@ -1,5 +1,6 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
+/// A description of the structure of a type.
 public enum Metatype {
 	/// A structural, or algebraic, type.
 	case Structural(Sum)
